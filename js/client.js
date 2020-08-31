908848f48f032ea80b3d14fc21204f84356bc650
unknown <cristian.lopez04@uptc.edu.co>
2020-08-31 13:26:26 -0500

    UPDATE

--- js/client.js
+++ js/client.js
@@ -76,3 +76,38 @@ socket.on('res:time', (data) => {
     const time = data.offset;
     convertSecondsToTime(time);
 });
+
+const reloj = document.getElementById('clock');
+var dialLines = document.getElementsByClassName('diallines');
+var clockEl = document.getElementsByClassName('reloj')[0];
+
+for (var i = 1; i < 60; i++) {
+  clockEl.innerHTML += "<div class='diallines'></div>";
+  dialLines[i].style.transform = "rotate(" + 6 * i + "deg)";
+}
+
+function fijarAgujas() {
+
+	const ahora = new Date();
+	const horas = ((hora + 11) % 12 + 1);
+	const minutos = minuto;
+	const segundos = segundo;
+	const horario = document.querySelector('.horario');
+	const minutero = document.querySelector('.minutero');
+	const segundero = document.querySelector('.segundero');
+	const gradosHora = horas * 30 + minutos * (360/720); // * 360 / 12
+	horario.style.transform = `rotate(${gradosHora}deg)`;
+	const gradosMinutos = minutos * 6 + segundos * (360/3600); // *360 / 60
+	minutero.style.transform = `rotate(${gradosMinutos}deg)`;
+	const gradosSegundos = segundos * 6;
+	if(gradosSegundos === 90) {
+        segundero.style.transition = 'none'
+    } else {
+        segundero.style.transition = ''
+    }
+    console.log("HORA: " + horas + ' MINUTO: ' + minutos + ' SEGUNDO: ' + segundos);
+	segundero.style.transform = `rotate(${gradosSegundos}deg)`;
+	reloj.style.display = "block";
+}
+
+setInterval(fijarAgujas, 1000);
